d62bcdb1a68eb3c8c90ed3567f14c8b79ec39d4c
unknown <dezaodoido@gmail.com>
2015-05-06 01:05:13 -0300

    init

---
+++ README.md
@@ -0,0 +1,36 @@
+# [![Imgur](http://i.imgur.com/FHjshUv.png)](https://github.com/webcaetano/jinx)
+
+[Jinx](https://github.com/webcaetano/jinx) module for handle error and submit to browser console
+
+## Instalation
+
+```
+npm install jinx-error-catcher
+```
+
+## Usage 
+```javscript
+require('jinx-error-catcher')(); // that's all
+// all errors will display on browser console
+
+// or with options
+
+require('jinx-error-catcher')({
+	deepDir:2, 
+	maxStack:3 
+});
+```
+
+## Options
+
+#### deepDir
+show up to 3 parents folder Like (/home/www/main.as).
+- Default 2;
+
+#### maxStack
+show 3 stacks of root error 
+- Default 3;
+
+---------------------------------
+
+The MIT [License](https://raw.githubusercontent.com/webcaetano/jinx-error-catcher/master/LICENSE.md)
---
+++ gulp/watch.js
@@ -0,0 +1,83 @@
+'use strict';
+
+var gulp = require('gulp');
+var flash = require('gulp-flash');
+var browserSync = require('browser-sync');
+var through = require('through2');
+var path = require('path');
+var fs = require('fs');
+var ext_replace = require('gulp-ext-replace');
+var jinxCompiler = require('jinx-compiler')
+
+var $ = require('gulp-load-plugins')({
+	pattern: ['del']
+});
+
+function isOnlyChange(event) {
+	return event.type === 'changed';
+}
+
+module.exports = function(options) {
+	var tmpFolder = path.join(options.tmp,'jinx');
+
+	gulp.task('watch', ['scripts:watch'], function () {
+
+		gulp.watch([options.src + '/*.html', 'bower.json'],function(){
+			gulp.start('inject',function(){
+				browserSync.reload();
+			});
+		});
+
+		gulp.watch([
+			options.src + '/{app,components,less}/**/*.css',
+			options.src + '/{app,components,less}/**/*.scss'
+		], function(event) {
+			if(isOnlyChange(event)) {
+				gulp.start('styles',function(){
+					browserSync.reload();
+				});
+			} else {
+				gulp.start('inject');
+			}
+		});
+
+
+		gulp.watch(options.src + '/{app,components}/**/*.html', function(event) {
+			browserSync.reload(event.path);
+		});
+	});
+
+	gulp.task('copy',function(){
+		return gulp.src([
+			'index.jinx',
+			options.src +'/**/*.{as,swc,jinx}',
+		])
+		.pipe(gulp.dest(tmpFolder));
+	});
+
+	gulp.task('build', ['copy'], function () {
+		var mainFile = path.join(tmpFolder,'app/test.jinx');
+
+		return gulp.src(options.src +'/app/test.jinx')
+		.pipe(through.obj(function (file, enc, callback) {
+			var resp = jinxCompiler(file);
+			file.contents = resp.contents;
+			file.swc = resp.swc;
+			callback(null,file);
+		}))
+		.pipe(ext_replace('.as'))
+		.pipe(gulp.dest(path.dirname(mainFile)))
+		.pipe(flash(options.src + '/app/flash/dist',{
+			'debug':true, // enable this for detailed errors
+			// 'library-path': [
+			// 	options.src + '/app/flash/libs'
+			// ]
+		}));
+	});
+
+	gulp.watch([options.src + '/**/*.{as,jinx,swc}','index.jinx'], function(event) {
+		gulp.start('build',function(){
+			browserSync.reload(event.path);
+		});
+	});
+};
